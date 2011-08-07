8c6c391b323a529a60b9615ab93dcd9cd1bee86c
Julian Kornberger <jk+github@digineo.de>
2011-08-07 19:53:43 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mediacrawler::Application.config.secret_token = '799175f7137830df88aaaaf732f3bde953822e7b317a16f4561d0b2c3f6509695a30bc48a5516f1825e7c486921534e04eb2423dcb22d577e37d5197fd719885'
