86f9f550705c8c6c8980b3cd3e95593a79bc5fd3
Julian Kornberger <jk+github@digineo.de>
2011-08-08 18:11:58 +0200

    root-route set up

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ VideoCrawler::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "resources#index"
 
   # See how all your routes lay out with "rake routes"
 
