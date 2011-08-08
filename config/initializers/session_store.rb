1b0a460e4caf66d47f2f531d77316ce619e77514
Julian Kornberger <jk+github@digineo.de>
2011-08-08 21:46:02 +0200

    renamed "VideoCrawler" into "MediaCrawler"

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-VideoCrawler::Application.config.session_store :cookie_store, :key => '_videocrawler_session'
+MediaCrawler::Application.config.session_store :cookie_store, :key => '_MediaCrawler_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
