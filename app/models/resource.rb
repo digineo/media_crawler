b93061aedd619a2b124125a93e7d3b19a75c4573
Julian Kornberger <jk+github@digineo.de>
2011-08-11 23:08:12 +0200

    server list added

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,15 +1,16 @@
 class Resource < ActiveRecord::Base
   
   FILE_PATTERN = /\.(avi|flv|mpe?g|mp[24]|mkv|wmv|)$/i
-  
   FIELD_PATTERN = /\w+:[\w\.-]+/
-  
   CHUNK_SIZE = 1024*50
   
   belongs_to :server
   
   include Resource::Metadata
   include Resource::Search
+  
+  scope :indexed, where(:indexed => true)
+  scope :non_indexed, where(:indexed => false)
   
   def uri
     server.uri_ftp + path[1..-1]
