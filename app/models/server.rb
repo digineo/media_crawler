7fe9ff69f91c2e2a08014ff48c6df7b3d51b619f
Julian Kornberger <jk+github@digineo.de>
2011-08-11 21:05:23 +0200

    chunk downloading implemented

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,5 @@
+require "net/ftp"
+
 class Server < ActiveRecord::Base
   
   has_many :resources
@@ -9,6 +11,7 @@ class Server < ActiveRecord::Base
     `mkdir -p '#{dir}'`
     `lftp '#{host_ftp}' -e "du -a" > #{file}`
     
+    files_count = 0
     f = File.open(file, "r") 
     f.each_line do |line|
       cols = line.strip.split("\t")
@@ -16,15 +19,34 @@ class Server < ActiveRecord::Base
       path = cols[1]
       path = path[1..-1] if path.starts_with?(".")
       
+      # check file pattern
       next unless Resource::FILE_PATTERN =~ path
       
+      # create/update resource
       resource = resources.find_or_initialize_by_path(path)
       resource.filesize = size
       resource.last_seen_at = now
       resource.save!
+      
+      files_count += 1
     end
     
     f.close
+    
+    files_count
+  end
+  
+  def update_metadata
+    ftp = Net::FTP.new(host_ftp)
+    begin
+      ftp.login
+      resources.non_indexed.find_each do |resource|
+        resource.download_chunk(ftp)
+        resource.update_metadata
+      end
+    ensure
+      ftp.close
+    end
   end
   
   def host_ftp
