c49affaee50f9fad7090007a2f6f9645ff405ae8
Julian Kornberger <jk+github@digineo.de>
2011-08-11 20:29:55 +0200

    Indexing of remote FTP files

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,4 +2,33 @@ class Server < ActiveRecord::Base
   
   has_many :resources
   
+  def update_files
+    now  = Time.now
+    dir  = "#{Rails.root}/data/servers/#{id}"
+    file = "#{dir}/filelist"
+    `mkdir -p '#{dir}'`
+    `lftp '#{host_ftp}' -e "du -a" > #{file}`
+    
+    f = File.open(file, "r") 
+    f.each_line do |line|
+      cols = line.strip.split("\t")
+      size = cols[0]
+      path = cols[1]
+      path = path[1..-1] if path.starts_with?(".")
+      
+      next unless Resource::FILE_PATTERN =~ path
+      
+      resource = resources.find_or_initialize_by_path(path)
+      resource.filesize = size
+      resource.last_seen_at = now
+      resource.save!
+    end
+    
+    f.close
+  end
+  
+  def host_ftp
+    URI.parse(uri_ftp).host
+  end
+  
 end
--- db/migrate/20110807174859_create_resources.rb
+++ db/migrate/20110807174859_create_resources.rb
@@ -4,13 +4,15 @@ class CreateResources < ActiveRecord::Migration
       t.references :server, :null => false
       t.string :path, :null => false
       t.column :filesize, 'bigint unsigned'
+      t.boolean :indexed, :null => false, :default => false
       t.text :metadata
-
+      t.datetime :last_seen_at
       t.timestamps :null => false
     end
     
     change_table :resources do |t|
       t.index [:server_id, :path]
+      t.index [:server_id, :last_seen_at]
     end
   end
 
