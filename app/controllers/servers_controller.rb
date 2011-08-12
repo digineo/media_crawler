344fdfcc6a42b7b493eece006e86e17a7bba6fc0
Julian Kornberger <jk+github@digineo.de>
2011-08-12 14:06:22 +0200

    stats to serverlist added

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -3,4 +3,8 @@ class ServersController < ApplicationController
   inherit_resources
   actions :index
   
+  def index
+    @stats = Resource.indexed.all(:select => 'count(*) count, sum(filesize) filesize')[0]
+  end
+  
 end
