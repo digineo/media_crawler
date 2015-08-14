f945032017c15e4a2ab0ef638115d6b856c2acb1
Julian Kornberger <jk+github@digineo.de>
2015-08-14 14:01:24 +0200

    Kill thin after deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,15 +27,12 @@ set :ssh_options,    user: fetch(:application)
 set :bundle_flags,   '--deployment --quiet --retry 3 --jobs 4'
 set :bundle_without, 'development deployment test'
 
-namespace :deploy do
+after 'deploy:publishing', 'deploy:restart'
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+namespace :deploy do
+  task :restart do
+    on roles(:web) do
+      execute "pkill -f thin"
     end
   end
-
 end
